8063eb48985be7d2535626e62e270d8de3810465
tkjacksonhod <tkjacksonhod@gmail.com>
2019-11-16 15:06:58 -0600

    Create debugFunctions.py

---
+++ functions/debugFunctions.py
@@ -0,0 +1,56 @@
+'''
+Created on Sep 24, 2019
+
+@author: amayamunoz
+'''
+
+'''
+For this assignment you should look at the functions created below and find the error.
+For each task, there will be one error that you must find and correct.
+Sometimes there will be an explanation of the problem and/or tips that can help you
+complete the tasks.
+EXAMPLE TASK:
+'''
+#EX)
+# This function should take in three numbers and add them together
+# Does not do desired function:
+# def multiply_three_numbers(num1, num2) :
+#    sumOfThreeNumbers = num1 * num2
+#    return sumOfThreeNumbers
+# Correct:
+def multiply_three_numbers(num1, num2, num3):
+    sumOfThreeNumbers = num1 + num2 + num3
+    return sumOfThreeNumbers
+'''
+END EXAMPLE
+'''
+
+#1)
+# This function should take in three numbers and multiply them together
+# Does not do desired function:
+# def multiply_three_numbers(num1, num2): 
+#    productOfThreeNumbers = num1 * num2
+#    return productOfThreeNumbers
+# Correct:
+def multiply_three_numbers(num1, num2, num3):
+    productOfThreeNumbers = num1*num2*num3
+    return productOfThreeNumbers
+#2) 
+# This function has the wrong naming conventions:
+# def Multiply by Two(num1):
+#    multiplied = num1 * 2
+#    return multiplied
+# Correct:
+def multiply_numbers(num):
+    multiplied = num * 2
+    return multiplied 
+#3) 
+# This function uses one more line than necessary, make it do the same function
+# but in one line
+# def divide_by_ten(num):
+#    divided = num / 10
+#    return divided
+# Correct:
+def divided_by_ten(num):
+    divided = num/10
+    return divided 
