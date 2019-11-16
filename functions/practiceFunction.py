ae2b03ad5a121ce300fb0b92a1eaf4b47dc23de8
tkjacksonhod <tkjacksonhod@gmail.com>
2019-11-16 12:29:34 -0600

    Create practiceFunction.py

---
+++ functions/practiceFunction.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 16, 2019
+
+@author: ITAUser
+'''
+def addition(num1, num2):
+    sumOfTwoNumbers = num1 + num2
+    return sumOfTwoNumbers
+
+sumOfTwoNumbers = addition(2, 5)
+
+def sum_three_numbers(num1, num2, num3):
+    sumOfNumbers = num1 + num2 + num3
+    return sumOfNumbers
+
+sumOfThreeNumbers = sum_three_numbers(5, 4, 1)
